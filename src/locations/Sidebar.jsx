09afe517665e2fbce76289a72b4a738c97b11459
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-04-07 13:18:56 -0400

    sidebar

--- src/locations/Sidebar.jsx
+++ src/locations/Sidebar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Paragraph } from '@contentful/f36-components';
-import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import React from 'react'
+import { Button } from '@contentful/f36-button'
+import { Stack } from '@contentful/f36-core'
+
+async function triggerBuildHook() {
+  try {
+    await fetch(`${process.env.REACT_APP_NETLIFY_BUILD_HOOK}`, {
+      method: 'POST',
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
 
 const Sidebar = () => {
-  const sdk = useSDK();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
+  return (
+    <Stack flexDirection='column' spacing='spacingM'>
+      <Button size='medium' isFullWidth onClick={triggerBuildHook}>
+        Build Netlify Preview
+      </Button>
+    </Stack>
+  )
+}
 
-  return <Paragraph>Hello Sidebar Component (AppId: {sdk.ids.app})</Paragraph>;
-};
-
-export default Sidebar;
+export default Sidebar
